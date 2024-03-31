b206ee43346d9549fc7e3b26ce688d93f21eea5d
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-03-30 19:47:14 -0500

    needed commit

--- project/api.py
+++
@@ -1,8 +0,0 @@
-from .models import Project
-from rest_framework import viewsets, permissions
-from .serializaers import ProjectSerializer
-
-class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = ProjectSerializer
